3d8b7596f3547b2a5a2525362056fc8772ba967d
mkotkov <maximkotkov@gmail.com>
2025-08-02 17:05:21 +0300

    logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import LogoSection from "./components/LogoSection.jsx"
+import NavBar from "./components/NavBar.jsx"
 import Hero from "./sections/Hero.jsx"
+import ShowcaseSection from "./sections/ShowcaseSection.jsx"
 
 const App = () => {
   return (
     <>
-       <Hero /> 
+      <NavBar />
+      <Hero /> 
+      <ShowcaseSection />
+       <LogoSection />
     </>
   )
 }
