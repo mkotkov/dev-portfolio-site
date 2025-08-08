f178492a4f03241a5ed8ac0bd775e6bfb6687778
mkotkov <maximkotkov@gmail.com>
2025-08-09 00:05:19 +0300

    tech block

--- src/components/TitleHeader.jsx
+++ src/components/TitleHeader.jsx
@@ -1,11 +1,15 @@
 const TitleHeader = ({ title, sub}) => {
   return (
     <div className="flex flex-col items-center gap-5">
-        <p>{sub}</p>
+      
+        <div className="hero-badge">
+          <p>{sub}</p>
+        </div>
     
         <div className="font-semibold md:text-5xl text-3xl text-center">
-            {title}
+          {title}
         </div>
+
     </div>   
   )
 }
